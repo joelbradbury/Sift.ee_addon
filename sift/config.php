5f876a2084f88533bd3691e8c9ba2948f1038963
Joel Bradbury <joelbradbury@gmail.com>
2013-04-26 16:06:45 +0100

    Bumped to version 1.2

--- sift/config.php
+++ sift/config.php
@@ -4,7 +4,7 @@
  * Sift Config File 
  *
  * @package         sift_ee_addon
- * @version         1.1
+ * @version         1.2.0
  * @author          Joel Bradbury ~ <joel@squarebit.co.uk>
  * @link            http://squarebit.co.uk/sift
  * @copyright       Copyright (c) 2013, Joel Bradbury
@@ -14,7 +14,7 @@ if ( ! defined('SIFT_NAME'))
 {
 	define('SIFT_NAME',         'Sift');
 	define('SIFT_CLASS_NAME',   'Sift');
-	define('SIFT_VERSION',      '1.2.a1');
+	define('SIFT_VERSION',      '1.2.0');
 	define('SIFT_DOCS',         'http://squarebit.co.uk/sift');
 	define('SIFT_DEBUG',        FALSE);
 }
